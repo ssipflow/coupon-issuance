526f3bf27326af25a9625766d2eaacb8af1b4bca
Nelson Kim <ssipflow@gmail.com>
2025-06-29 16:59:09 +0900

    improve k6 stress test for guarantee 1000 TPS

--- test/k6/stress/stress_test.js
+++ test/k6/stress/stress_test.js
@@ -1,14 +1,22 @@
 import http from 'k6/http';
-import { check, sleep } from 'k6';
+import { check } from 'k6';
 
 export const options = {
-    vus: 1000,
-    duration: '1s',
+    scenarios: {
+        constant_rate_test: {
+            executor: 'constant-arrival-rate',
+            rate: 1000, // 1000 RPS
+            timeUnit: '1s',
+            duration: '1s',
+            preAllocatedVUs: 1000,
+            maxVUs: 2000,
+        },
+    },
 };
 
 export default function () {
     const userId = Math.floor(Math.random() * 100000) + 1;
-    const campaignId = 2;
+    const campaignId = 4;
 
     const payload = JSON.stringify({
         campaignId: campaignId,
@@ -20,10 +28,12 @@ export default function () {
     };
 
     const res = http.post('http://localhost:8080/coupon.v1.CouponService/IssueCoupon', payload, { headers });
+    if (res.status !== 200) {
+        const data = JSON.parse(res.body);
+        console.log(`Failure issuing coupon for user ${userId}: ${data.message || 'Unknown error'}`);
+    }
 
     check(res, {
         'status is 200': (r) => r.status === 200,
     });
-
-    sleep(0.1);
 }
